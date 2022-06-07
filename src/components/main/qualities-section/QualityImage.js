93a3425b170d1f68d90e67eb1ed67b0f5093b53c
Richard Aiello <richaiello17@gmail.com>
2022-06-07 13:53:14 -0400

    JSX for section qualities.

---
+++ src/components/main/qualities-section/QualityImage.js
@@ -0,0 +1,5 @@
+export default function QualityImage(props) {
+  return (
+    <img src={props.src} alt="" className="qualities__icon" />
+  );
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,6 +3,7 @@ import Footer from "../components/footer/Footer";
 import HeroBtn from "../components/hero/HeroBtn";
 import HeroImage from "../components/hero/HeroImage";
 import DesignCard from "../components/main/design-section/DesignCard";
+import Quality from "../components/main/qualities-section/Quality";
 
 export default function Home() {
   return (
@@ -22,11 +23,37 @@ export default function Home() {
         image={<HeroImage />}
       />
       <main className="main">
-      <section className="design container">
-        <DesignCard web={true} />
-        <DesignCard app={true} />
-        <DesignCard graphic={true} />
-      </section>
+        <section className="design container">
+          <DesignCard web={true} />
+          <DesignCard app={true} />
+          <DesignCard graphic={true} />
+        </section>
+        <section className="qualities container">
+          <Quality
+            text={
+              `Each project starts with an in-depth brand research to ensure 
+              we only create products that serve a purpose. We merge
+              art, design, and technology into exciting new solutions.`
+            }
+            passionate={true}
+          />
+          <Quality
+            text={
+              `Everything that we do has a strategic purpose. We use an agile 
+              approach in all of our projects and value customer collaboration. 
+              It guarantees superior results that fulfill our clients’ needs.`
+            }
+            resourceful={true}
+          />
+          <Quality
+            text={
+              `We are a group of enthusiastic folks who know how to put people 
+              first. Our success depends on our customers, and we
+              strive to give them the best experience a company can provide.`
+            }
+            friendly={true}
+          />
+        </section>
       </main>
       <Footer footerClass="footer" />
     </>
