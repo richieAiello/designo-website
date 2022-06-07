b55bdfc3734d9dfa5ef61caec4f1adf02fd08148
Richard Aiello <richaiello17@gmail.com>
2022-06-07 12:58:46 -0400

    Refactored design cards into a dynamic component.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import Hero from "../components/hero/Hero";
 import Footer from "../components/footer/Footer";
 import HeroBtn from "../components/hero/HeroBtn";
 import HeroImage from "../components/hero/HeroImage";
-import Design from "../components/main/Design";
+import DesignCard from "../components/main/design-section/DesignCard";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
           `Award-winning custom designs and digital branding solutions`
         }
         text={
-          `With over 10 years in the industry, we are experienced in 
+          `With over 10 years in the industry, we are experienced in
           creating fully responsive websites, app design, 
           and engaging brand experiences. Find out more about our services.`
         }
@@ -22,11 +22,11 @@ export default function Home() {
         image={<HeroImage />}
       />
       <main className="main">
-        <Design 
-          webCard={true}
-          appCard={true}
-          graphicCard={true}
-        />
+      <section className="design container">
+        <DesignCard web={true} />
+        <DesignCard app={true} />
+        <DesignCard graphic={true} />
+      </section>
       </main>
       <Footer footerClass="footer" />
     </>
