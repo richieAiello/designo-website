da935d17d1b3d22b84843864c1d9bfb74801f949
Richard Aiello <richaiello17@gmail.com>
2022-06-08 12:57:38 -0400

    JSX for ProjectsCard.js.

--- src/components/main/projects-section/ProjectsCard.js
+++ src/components/main/projects-section/ProjectsCard.js
@@ -1,3 +1,31 @@
+import images from "./ProjectsImages";
+
 export default function ProjectsCard(props) {
-  
+  let cardImage;
+  let cardHeading;
+
+  images.filter(image => {
+    if (image.id === props.card) {
+      cardImage = image.src;
+      cardHeading = image.heading;
+    }
+  })
+
+  return (
+    <div className="projects__card">
+      <div className="projects__context">
+        <h2 className="projects__heading bold">
+          {cardHeading}
+        </h2> 
+        <p className="projects__text">
+          {props.text}
+        </p>
+      </div>
+      <img
+        src={cardImage}
+        alt=""
+        className="image image--projects"
+      />
+    </div>
+  );
 }
--- src/routes/WebDesign.js
+++ src/routes/WebDesign.js
@@ -1,4 +1,5 @@
 import Hero from "../components/hero/Hero";
+import ProjectsCard from "../components/main/projects-section/ProjectsCard";
 import Footer from "../components/footer/Footer";
 
 export default function WebDesign() {
@@ -15,6 +16,16 @@ export default function WebDesign() {
           and bring memorable brand experiences.`
         }
       />
+      <main className="main">
+        <section className="projects container">
+          <ProjectsCard 
+            card="express" 
+            text={
+              `A multi-carrier shipping website for ecommerce businesses`
+            }
+          />
+        </section>
+      </main>
       <Footer footerClass="footer" />
     </>
   );
