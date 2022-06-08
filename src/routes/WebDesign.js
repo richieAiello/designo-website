4d07dd3267f12a0141a7f4084efc96600501baff
Richard Aiello <richaiello17@gmail.com>
2022-06-08 10:30:06 -0400

    Hero and footer for Design pages.

--- src/routes/WebDesign.js
+++ src/routes/WebDesign.js
@@ -1,9 +1,21 @@
+import Hero from "../components/hero/Hero";
 import Footer from "../components/footer/Footer";
 
 export default function WebDesign() {
   return (
     <>
-      <Footer className="footer" />
+      <Hero 
+        heroClass="hero--design"
+        wrapperClass="hero__wrapper--design"
+        heading={
+          `Web Design`
+        }
+        text={
+          `We build websites that serve as powerful marketing tools 
+          and bring memorable brand experiences.`
+        }
+      />
+      <Footer footerClass="footer" />
     </>
   );
 }
