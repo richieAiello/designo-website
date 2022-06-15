cefe31969595004630353152e576f5e7ceda4609
Richard Aiello <richaiello17@gmail.com>
2022-06-15 14:28:00 -0400

    Styles for section design--home on desktop.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
           image={<HeroImage />}
         />
         <section className="design design--home container">
-          <DesignCard web={true} />
+          <DesignCard web={true} large={true} />
           <DesignCard app={true} />
           <DesignCard graphic={true} />
         </section>
